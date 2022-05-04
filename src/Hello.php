e64c59ba2fc888b2c624985a5279ea295c454de5
Bishal Baaniya <bishalbaaniya@gmail.com>
2022-05-04 09:09:08 -0400

    test

---
+++ src/Hello.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * (c) Bishal Baaniya <baaniyb@miamioh.edu>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace baaniyb\MyFirstPackage;
+
+class Hello
+{
+    public function say($name)
+    {
+        return "Hello, $name!";
+    }
+}
+
+?>
